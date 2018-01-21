cad0119901c160bbfa1a53c69f5e9f28fec57460
JonathanPucci <jonathanpucci06@gmail.com>
2018-01-21 21:21:12 +0100

    gmaps working finally fuck yeah

---
+++ LetsSportNow/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import MapView from "react-native-maps";
+import { AppRegistry, StyleSheet, Text, View, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
+      markers: []
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
+      </View>
+    );
+  }
+}
